c72eecf75a7cffe45b1cca05da5eb91b0a81922e
K. <126347036+FiendPopsicle@users.noreply.github.com>
2023-12-20 15:00:05 -0500

    Configuration cleanup and updates

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,44 @@
-
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+
+  // This option controls if and how source maps are generated.
+  // Consider using 'source-map' or 'hidden-source-map' for production
+  devtool: 'source-map',
+
+  plugins: [
+    // CleanWebpackPlugin to clean the build folder
+    new CleanWebpackPlugin(),
+
+    // MiniCssExtractPlugin to extract CSS into separate files
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+
+  optimization: {
+    minimizer: [
+      // TerserPlugin to minify JavaScript
+      new TerserPlugin({
+        parallel: true,
+      }),
+
+      // CssMinimizerPlugin to optimize and minify CSS
+      new CssMinimizerPlugin(),
+    ],
+
+    // SplitChunksPlugin configuration, if needed
+    splitChunks: {
+      chunks: 'all',
+    },
+
+    // RuntimeChunk configuration, if needed
+    runtimeChunk: 'single',
+  },
 });
